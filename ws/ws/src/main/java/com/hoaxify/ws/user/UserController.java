7c9f61bb99070962ad56af9f5f68c296edd094c1
26518304542 <ozgurcimen1@gmail.com>
2024-11-15 12:11:25 +0300

    first commit

---
+++ ws/ws/src/main/java/com/hoaxify/ws/user/UserController.java
@@ -0,0 +1,67 @@
+package com.hoaxify.ws.user;
+
+
+
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hoaxify.ws.error.ApiError;
+import com.hoaxify.ws.shared.GenericMessage;
+import com.hoaxify.ws.shared.Messages;
+import com.hoaxify.ws.user.dto.UserCreate;
+import com.hoaxify.ws.user.exception.NotUniqueEmailException;
+
+import jakarta.validation.Valid;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+
+    @PostMapping("/api/v1/users")
+    GenericMessage createUser(@Valid @RequestBody UserCreate user){
+        userService.save(user.toUser());
+        String message = Messages.getMessageForLocale("hoaxify.create.user.success.message", LocaleContextHolder.getLocale());
+        return new GenericMessage(message);
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    ResponseEntity<ApiError> handleMethodArgNotValidEx(MethodArgumentNotValidException exception){
+        ApiError apiError = new ApiError();
+        apiError.setPath("/api/v1/users");
+        apiError.setStatus(400);
+        String message = Messages.getMessageForLocale("hoaxify.error.validation", LocaleContextHolder.getLocale());
+        apiError.setMessage(message);
+        var validationErrors = exception.getBindingResult().getFieldErrors().stream().collect(Collectors
+        .toMap(FieldError::getField,
+               FieldError::getDefaultMessage,
+               (existing, replacing) -> existing));
+
+        apiError.setValidationErrors(validationErrors);
+        return ResponseEntity.badRequest().body(apiError);
+    }
+
+
+    @ExceptionHandler(NotUniqueEmailException.class)
+    ResponseEntity<ApiError> handleUniqueEmailFix(NotUniqueEmailException exception){
+        ApiError apiError = new ApiError();
+        apiError.setPath("/api/v1/users");
+        apiError.setMessage(exception.getMessage());
+        apiError.setStatus(400);
+        apiError.setValidationErrors(exception.getValidationErrors());
+        return ResponseEntity.badRequest().body(apiError);
+    }
+
+}
