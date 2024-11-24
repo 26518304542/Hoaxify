72a37843cfd5c27ffcac58420a974f76e12dd362
26518304542 <ozgurcimen1@gmail.com>
2024-11-24 23:32:13 +0300

    navigation-to-user-page

--- frontend/frontend/src/pages/Home/components/UserListItem.jsx
+++ frontend/frontend/src/pages/Home/components/UserListItem.jsx
@@ -1,14 +1,20 @@
 import defaultFileImage from "@/assets/profile.png"
+import { Link } from "react-router-dom"
 
 export function UserListItem({ user }) {
     return (
-        <li
-            className="list-group-item list-group-item-action">
+        <Link
+            className="list-group-item list-group-item-action"
+            to={`/user/${user.id}`}
+            style={{ textDecoration: "none" }}
+            >
             <img
                 src={defaultFileImage}
                 width="30"
                 className="img-fluid rounded-circle shadow-sm"
             />
             <span className="ms-2">{user.username}</span>
-        </li>);
+        </Link>
+
+    );
 }
--- frontend/frontend/src/router/index.js
+++ frontend/frontend/src/router/index.js
@@ -4,6 +4,7 @@ import { Home } from "@/pages/Home";
 import { SignUp } from "@/pages/SignUp";
 import {Activation} from "@/pages/Activation";
 import App from "@/App";
+import { User } from "@/pages/User";
 
 
 export default createBrowserRouter([
@@ -23,6 +24,10 @@ export default createBrowserRouter([
         {
             path: "/activation/:token",
             Component: Activation,
+        },
+        {
+            path: "/user/:id",
+            Component: User
         }
     ]
 }
