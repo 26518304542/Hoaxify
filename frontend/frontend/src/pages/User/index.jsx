71d168a58f37420bc135fd9d8c23053f8c72917c
26518304542 <ozgurcimen1@gmail.com>
2024-11-25 15:19:59 +0300

    user-page

--- frontend/frontend/src/pages/User/index.jsx
+++ frontend/frontend/src/pages/User/index.jsx
@@ -1,3 +1,55 @@
+import { Component } from "react";
+import { getUser } from "./api";
+import { Alert } from "@/shared/components/Alert";
+import { Spinner } from "@/shared/components/Spinner";
+import { useParams } from "react-router-dom";
+
+
+
+export class UserClass extends Component{
+
+    state = {
+        user: null,
+        apiProgress: null,
+        error: null
+    }
+
+    async componentDidMount() {
+        this.setState({ apiProgress:true })
+        try{
+            const response = await getUser(this.props.id);
+            this.setState({
+                user: response.data,
+            });
+        }
+        catch(axiosError){
+            this.setState({
+                error: axiosError.response.data.Message,
+            })
+        }
+        finally{
+            this.setState({
+                apiProgress:false
+            });
+        }
+    }
+
+    render() {
+        return (
+            <>
+            {this.state.user && <h1>this.state.user.username</h1>}
+            {this.state.apiProgress &&
+                (<Alert styleType="danger" center>
+                    <Spinner />
+                </Alert>)
+            }
+            {this.state.error && <Alert styleType="danger" center>this.state.error</Alert>}
+            </>
+        );
+    }
+}
+
 export function User(){
-    return <div>User Page</div>
+    const { id } = useParams();
+    return <UserClass id={id} />;
 }
